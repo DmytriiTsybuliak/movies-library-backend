897da56f667ef9c1104eefb1b32b2e39952a91fc
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-11-09 01:30:35 +0300

    initMongo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
+import { initMongoDB } from "./db/initMongoDB.js";
 import { setupServer } from "./server.js";
 
-
-
 const bootstrap = async () => {
+    await initMongoDB();
     setupServer();
 };
 
