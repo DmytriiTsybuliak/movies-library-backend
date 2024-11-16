be6904244d4b3ee4317f8539a2f983a6e869b8b7
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-11-17 02:44:08 +0300

    validation for register request

--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 // import controllerWr from "../utils/controllerWr.js";
 import { loginController, registerController } from "../controllers/authController.js";
+import { validateMiddleware } from "../middlewares/validateMiddleware.js";
+import { registerUserSchema } from "../validation/auth.js";
 
 const authRouter = Router();
 
-authRouter.post('/register', registerController);
+authRouter.post('/register', validateMiddleware(registerUserSchema), registerController);
 authRouter.post('/login', loginController);
 
 export default authRouter;
