8c9c6c81a768c437abaebb94844cce7dc41601ff
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-11-30 21:08:26 +0300

    added middleware to chech authority

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,7 @@
-import { User } from "../db/models/userModel";
-import jwt from 'jsonwebtoken';
+
+import { User } from '../db/models/userModel.js';
+import { SessionsCollection } from '../db/models/session.js';
+import createHttpError from 'http-errors';
 
 export const protectMW = async (req, res, next) => {
     try {
@@ -13,10 +15,32 @@ export const protectMW = async (req, res, next) => {
             return res.status(401).json({ message: 'You are not logged in!' });
         }
 
+        const session = await SessionsCollection.findOne({ accessToken: token });
+        if (!session) {
+            next(createHttpError(401, 'Session not found'));
+            return;
+        }
+
+        const isAccessTokenExpired =
+            new Date() > new Date(session.accessTokenValidUntil);
+
+        if (isAccessTokenExpired) {
+            next(createHttpError(401, 'Access token expired'));
+        }
+
+        const user = await User.findById(session.userId);
+
+        if (!user) {
+            next(createHttpError(401));
+            return;
+        }
+
+        req.user = user;
         // Проверка и декодирование токена
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.id);
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        // req.user = await User.findById(decoded.id);
         next();
+        // eslint-disable-next-line no-unused-vars
     } catch (error) {
         res.status(401).json({ message: 'Invalid token' });
     }
