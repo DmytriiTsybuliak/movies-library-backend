2ab562759d4fd4e8149b100c6136f73f703f5760
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-12-03 01:20:08 +0300

    added reset email and password routes

--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import controllerWr from "../utils/controllerWr.js";
-import { loginController, logoutController, refreshUserSessionController, registerController } from "../controllers/authController.js";
+import { loginController, logoutController, refreshUserSessionController, registerController, requestResetEmailController, resetPassController } from "../controllers/authController.js";
 import { validateMiddleware } from "../middlewares/validateMiddleware.js";
-import { loginUserSchema, registerUserSchema } from "../validation/auth.js";
+import { loginUserSchema, registerUserSchema, requestResetEmailSchema, resetPassSchema } from "../validation/auth.js";
 
 const authRouter = Router();
 
@@ -10,5 +10,7 @@ authRouter.post('/register', validateMiddleware(registerUserSchema), controllerW
 authRouter.post('/login', validateMiddleware(loginUserSchema), controllerWr(loginController));
 authRouter.post('/logout', controllerWr(logoutController));
 authRouter.post('/refresh', controllerWr(refreshUserSessionController));
+authRouter.post('/request-reset-email', validateMiddleware(requestResetEmailSchema), controllerWr(requestResetEmailController));
+authRouter.post('/reset-password', validateMiddleware(resetPassSchema), controllerWr(resetPassController));
 
 export default authRouter;
