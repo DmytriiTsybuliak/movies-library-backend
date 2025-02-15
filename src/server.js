899fe09b76ac749ca7eec3fe7020126b5b9d8604
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2025-02-16 00:43:18 +0300

    headers

--- src/server.js
+++ src/server.js
@@ -15,6 +15,13 @@ export const setupServer = () => {
         credentials: true, // turns on credentials
     };
     app.use(cors(corsOptions));
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        res.header("Access-Control-Allow-Credentials", "true");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        next();
+    });
     app.use(cookieParser());
 
 
